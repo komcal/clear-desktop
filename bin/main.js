f674938ac7c743120f401b1051c629b525c3997d
Jirachai C <antronic.inc@gmail.com>
2018-10-02 18:12:42 +0700

    Made folder name looks simpler

--- bin/main.js
+++ bin/main.js
@@ -1,15 +1,30 @@
 #!/usr/bin/env node
-var shell = require("shelljs");
+const shell = require("shelljs");
+
+// For future feature, custom folder name structure
+// function formatFolder() {}
+
+function leadingZero(num) {
+  if (num < 10) {
+    return '0' + num;
+  }
+  return num.toString();
+}
+
 shell.cd();
 shell.cd('Desktop');
 
 const currTime = new Date();
+// Made folder name looks simpler
+const folderName = currTime.getFullYear() + '-' + leadingZero(currTime.getMonth() + 1) + '-' + leadingZero(currTime.getDate());
 
-shell.exec('mkdir "' + currTime + '"');
-console.log('create folder', currTime);
+shell.exec('mkdir "' + folderName + '"');
+console.log('create folder: ', folderName);
+
+const folderLocation = shell.pwd().toString() + '/' + folderName;
 
 shell.ls('*').forEach(function(file) {
-  shell.mv(file, currTime + '/');
+  shell.mv(file, folderLocation + '/');
   console.log(file,'has been moved.');
 });
 
